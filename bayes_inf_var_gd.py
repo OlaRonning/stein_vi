UPDATED: gradient computation to use correct shapes

bayes_inf_var_gd.py
@@ -16,20 +16,22 @@ def gaussian_pdf(mu, sigma):
 
 if __name__ == '__main__':
     # Initial toy target distribution
-    pdf_1 = gaussian_pdf(to_tensor([0.0]), to_tensor([1.0]))
-    pdf_2 = gaussian_pdf(to_tensor([3.0]), to_tensor([0.5]))
+    pdf_2 = gaussian_pdf(to_tensor(-2.0), to_tensor(1.))
+    pdf_1 = gaussian_pdf(to_tensor(2.0), to_tensor(1.))
     target_dist = lambda x: 1 / 3 * pdf_1(x) + 2 / 3 * pdf_2(x)
 
     ### INPUT ###
 
     initial_dist = Normal(to_tensor([-10.]), to_tensor([1.]))  # could be arbitrary (not all zeros)
-    num_particles = 100  # number of particles
+    num_particles = 49  # number of particles
     particles = initial_dist.sample((num_particles,)).squeeze().unsqueeze(1)
+    # particles = torch.ones((num_particles,1), dtype=torch.float)
 
+    print(pdf_1(particles))
     ### ALGORITHM ###
 
-    plot_system(target_dist, particles)
-    sgb = SteinVariationalGradientDescent(num_steps=2000, step_size=0.5, kernel='rbf', target_dist=target_dist)
+    # plot_system(particles)
+    sgb = SteinVariationalGradientDescent(num_steps=500, step_size=1., kernel='rbf', target_dist=target_dist)
     particles = sgb.execute(particles)
-
-    plot_system(target_dist, particles)
+    print(particles)
+    plot_system(particles)

stein_variational_gradient_descent.py
@@ -1,16 +1,17 @@
 import torch
 from functools import reduce
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 class SteinVariationalGradientDescent:
-    def __init__(self, num_steps, step_size, target_dist, fudge_factor=1e-8, kernel='rbf'):
+    def __init__(self, num_steps, step_size, target_dist, fudge_factor=1e-8, kernel='rbf', viz=True, alpha=0.9):
         self.num_steps = num_steps
-        if type(step_size) == float:  # check propertype
-            # make constant function
-            eta = step_size
-            step_size = lambda _: eta
+        self.step_size = step_size
         self.fugdge_factor = fudge_factor
         self.step_size = step_size
+        self.viz = viz
+        self.alpha = alpha
         if kernel == 'rbf':
             self.kernel = self._rbf_kernel
         else:
@@ -22,22 +23,32 @@ class SteinVariationalGradientDescent:
         # add momentum term
         n = particles.shape[0]  # [n,..]
 
-        gradient_history = torch.zeros((n,1), dtype=torch.float)
+        gradient_history = torch.zeros((n, 1), dtype=torch.float)
         for step in range(self.num_steps):
             grad = self._functional_gradient(particles, n)
-            gradient_history += grad ** 2
+            gradient_history *= self.alpha
+            gradient_history += ((not bool(step)) + bool(step) * (1 - self.alpha)) * grad ** 2
             rescale = 1 / torch.sqrt(gradient_history + self.fugdge_factor)
-            particles += self.step_size(step) * rescale * grad
+            particles += self.step_size * rescale * grad
+            if self.viz and step % 10 == 0:
+                plt.clf()
+                sns.kdeplot(particles.squeeze().numpy())
+                # plt.hist(target, bins=50, density=True)
+                plt.show(block=False)
+                plt.pause(0.01)
 
         return particles
 
     def _functional_gradient(self, particles, n):  # phi^{\star}
-        grad_kernel = self._grad(self.kernel)(particles)  # [n,n]
-        kernel = self.kernel(particles)  # [n,n]
-        grad_logprob = self._grad(torch.log, self.target_dist)(particles).expand((n, n))  # [n,n]
-        phi = kernel * grad_logprob + grad_kernel
-        result = 1 / n * torch.sum(phi, dim=1)  # emperical average
-        return result.unsqueeze(1)
+        kernel = self.kernel(particles).sum(1).unsqueeze(1)  # [n]
+        grad_kernel = self._grad(self.kernel)(particles)  # [n]
+        grad_logprob = self._grad(torch.log, self.target_dist)(particles)  # [n]
+        attractive = kernel * grad_logprob
+        repulsive = grad_kernel
+        phi = attractive + repulsive
+        phi[phi != phi] = 0
+        result = phi/n  # emperical average
+        return result
 
     @staticmethod
     def _grad(*args):
@@ -56,11 +67,10 @@ class SteinVariationalGradientDescent:
         pw_dists = torch.pdist(x) ** 2  # Upper triangle
         med = torch.median(pw_dists)
         return SteinVariationalGradientDescent._upper_tria_to_full(n, torch.exp(
-            -torch.sqrt(torch.tensor(n, dtype=float)) / med ** 2 * pw_dists))
+            -(torch.log(torch.tensor(n, dtype=float)) / (med ** 2 + 1e-8)) * pw_dists))
 
     @staticmethod
     def _upper_tria_to_full(n, triag_vec):
-        # TODO: make smarter
         matrix = torch.zeros((n, n), dtype=triag_vec.dtype)
         indices = torch.triu_indices(n, n, 1)
         matrix[indices[0], indices[1]] = triag_vec